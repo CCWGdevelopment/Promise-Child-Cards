fd68049ea5e90ce6d6788467afdc14fa11798cd1
mhoupt856 <marcus@ccwg.org>
2018-04-17 15:33:41 -0700

    added (and now saved) camera controller for CreateCardVC

--- Promise Child Cards/View Controllers/CreateCardVC.swift
+++ Promise Child Cards/View Controllers/CreateCardVC.swift
@@ -7,9 +7,18 @@
 //
 
 import UIKit
+import MobileCoreServices
 
 class CreateCardVC: UIViewController {
-
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var ageTextField: UITextField!
+    @IBOutlet weak var countryTextField: UITextField!
+    @IBOutlet weak var numberTextField: UITextField!
+    @IBOutlet weak var descripTextView: UITextView!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    private var imagePicker: UIImagePickerController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -25,14 +34,47 @@ class CreateCardVC: UIViewController {
         self.dismiss(animated: false, completion: nil)
     }
     
-    /*
-    // MARK: - Navigation
+    @IBAction func takePhotoButton(_ sender: Any) {
+        
+        imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        
+        let actionSheet = UIAlertController(title: "Photo Source", message: "Choose a source", preferredStyle: .actionSheet)
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            
+        actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: { (action:UIAlertAction) in
+            self.present(self.imagePicker, animated: true, completion: nil)
+            }))
+        }
+        actionSheet.addAction(UIAlertAction(title: "Photo Library", style: .default, handler: { (action:UIAlertAction) in
+            self.present(self.imagePicker, animated: true, completion: nil)
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        self.present(actionSheet, animated: true, completion: nil)
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+
     }
-    */
+    
 
+}
+
+extension CreateCardVC: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let mediaType = info[UIImagePickerControllerMediaType] as! String
+        
+        if mediaType == (kUTTypeImage as String) {
+            self.imageView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+        } else {
+//            self.dismiss(animated: true, completion: nil)
+        }
+        self.dismiss(animated: true, completion: nil)
+    }
 }
